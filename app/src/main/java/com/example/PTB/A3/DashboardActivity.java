594e90ae950aed9b02b65d84990417a142eca697
daeng1502 <daengfebrino01@gmail.com>
2022-09-15 21:21:01 +0700

    Testing OnClicked Button

--- app/src/main/java/com/example/PTB/A3/DashboardActivity.java
+++ app/src/main/java/com/example/PTB/A3/DashboardActivity.java
@@ -6,14 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void dashboardOnClicked(View view) {
+        setContentView(R.layout.activity_dashboard);
     }
 }
--- app/src/main/java/com/example/PTB/A3/LoginActivity.java
+++ app/src/main/java/com/example/PTB/A3/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.PTB.A3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void dashboardOnClicked(View view) {
+        Intent dashboardOnClicked = new Intent(this, DashboardActivity.class);
+        startActivity(dashboardOnClicked);
     }
 }
