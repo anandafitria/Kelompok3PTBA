a8e5ac22db15504a9b89b1d6c02943e1720ebcdc
anandaftrr <anandafitria.unand@gmail.com>
2022-09-16 10:34:31 +0700

    ULANG

---
+++ app/src/main/java/com/example/PTB/A3/ProfilActivity.java
@@ -0,0 +1,14 @@
+package com.example.PTB.A3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
